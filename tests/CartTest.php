8f26b86a73aceb4ecb9f9b9f96d7a07e15140c84
John Dent <john.dent@osaris.co.uk>
2019-11-25 07:22:16 +0000

    Update test case & travis.yml

--- tests/CartTest.php
+++ tests/CartTest.php
@@ -6,7 +6,7 @@ use Hassansin\DBCart\Models\CartLine;
 class CartTest extends Orchestra\Testbench\TestCase
 {
 
-    protected function setUp()
+    protected function setUp(): void
     {
         parent::setUp();
         $this->loadLaravelMigrations(['--database' => 'testbench']);
